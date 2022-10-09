4ed2c0288bb67499f95667870b01107557b23ca2
TA-CR <115377669+TA-CR@users.noreply.github.com>
2022-10-09 05:11:16 +0100

    Initial commit

---
+++ questions.js
@@ -0,0 +1,558 @@
+questions = [
+  {
+    question:
+      "I prefer wearing women's clothes (skirts, dresses, crop tops, thigh highs etc).",
+    effect: {
+      econ: 10,
+      dipl: 0,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question:
+      "I like thinking about how other's view me when I wear women's clothes.",
+    effect: {
+      econ: 5,
+      dipl: 0,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question: "Women's clothes should be considered gender neutral.",
+    effect: {
+      econ: 0,
+      dipl: 0,
+      govt: -10,
+      scty: 0
+    }
+  },
+  {
+    question:
+      "I would enjoy participating in a partner or friend's hobbies even if they were more masculine than I'm used to (ex. sports).",
+    effect: {
+      econ: -5,
+      dipl: 0,
+      govt: -5,
+      scty: 0
+    }
+  },
+  {
+    question: "My personality is generally feminine.",
+    effect: {
+      econ: 10,
+      dipl: 0,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question: "People say I have a feminine voice.",
+    effect: {
+      econ: 5,
+      dipl: 0,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question:
+      "I have considered or undergone voice training to sound more feminine.",
+    effect: {
+      econ: 10,
+      dipl: 0,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question: "I think of myself as a femboy, roseboy or effeminate male.",
+    effect: {
+      econ: 5,
+      dipl: 0,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question: "I think of myself as a twink or twunk.",
+    effect: {
+      econ: -5,
+      dipl: 0,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question: "I don't see myself as a man, or see myself as a 'non-man'.",
+    effect: {
+      econ: 10,
+      dipl: 0,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question:
+      "I do- or would like to- diet or exercise to look more feminine than I do currently.",
+    effect: {
+      econ: 5,
+      dipl: 0,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question:
+      "In an ideal world, I could take a pill, wake up, and be more feminine than most women",
+    effect: {
+      econ: 10,
+      dipl: 0,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question:
+      "I would like to eliminate all traces of masculinity from my body.",
+    effect: {
+      econ: 10,
+      dipl: 0,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question: "I have fantasized about being female.",
+    effect: {
+      econ: 10,
+      dipl: 0,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question:
+      "Having a lot of visible facial hair is a nighmare scenario for me.",
+    effect: {
+      econ: 10,
+      dipl: 0,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question: "I prefer wearing men's wear (suits, khaki pants etc).",
+    effect: {
+      econ: -10,
+      dipl: 0,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question: "Men's clothes should be considered gender neutral.",
+    effect: {
+      econ: 0,
+      dipl: 0,
+      govt: -5,
+      scty: 0
+    }
+  },
+  {
+    question:
+      "I like thinking about how other's view me when I wear men's wear.",
+    effect: {
+      econ: -5,
+      dipl: 0,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question:
+      "I would enjoy participating in a partner or friend's hobbies even if they were more feminine than I'm used to (ex. fashion).",
+    effect: {
+      econ: -5,
+      dipl: 0,
+      govt: -5,
+      scty: 0
+    }
+  },
+  {
+    question: "My personality is generally masculine.",
+    effect: {
+      econ: -10,
+      dipl: 0,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question: "People say I have a masculine voice.",
+    effect: {
+      econ: -5,
+      dipl: 0,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question:
+      "I have considered or undergone voice training to sound more masculine.",
+    effect: {
+      econ: -10,
+      dipl: 0,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question: "I consider myself as a gender conforming man.",
+    effect: {
+      econ: -5,
+      dipl: 0,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question: "I see myself, or my ideal self, as very masculine.",
+    effect: {
+      econ: -10,
+      dipl: 0,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question:
+      "I do- or would like to- diet or exercise to look more masculine than I do currently.",
+    effect: {
+      econ: -10,
+      dipl: 0,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question:
+      "In an ideal world, I could take a pill, wake up, and be more masculine than most men.",
+    effect: {
+      econ: -10,
+      dipl: 0,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question: "I don't mind facial hair on myself.",
+    effect: {
+      econ: -5,
+      dipl: 0,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question: "I would like more facial hair.",
+    effect: {
+      econ: -10,
+      dipl: 0,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question: "I am sexually attracted to masculine men.",
+    effect: {
+      econ: 0,
+      dipl: 25,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question: "I am sexually attracted to twinks.",
+    effect: {
+      econ: 0,
+      dipl: 10,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question: "I am sexually attracted to femboys, roseboys or effemiante men.",
+    effect: {
+      econ: 0,
+      dipl: -5,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question: "The more feminine the boy, the more attractive I find them.",
+    effect: {
+      econ: 0,
+      dipl: -10,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question: "I am sexually attracted to feminine women.",
+    effect: {
+      econ: 0,
+      dipl: -25,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question: "I am sexually attracted to tomboys.",
+    effect: {
+      econ: 0,
+      dipl: 5,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question: "I am sexually attracted to butch or muscular women.",
+    effect: {
+      econ: 0,
+      dipl: 10,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question:
+      "I prefer my sexual partner's to have trimmed or shaved body hair.",
+    effect: {
+      econ: 0,
+      dipl: -5,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question: "I don't mind if my partner is hairy.",
+    effect: {
+      econ: 0,
+      dipl: 5,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question:
+      "I prefer my partner's to be hairy, espcially chests or back hair.",
+    effect: {
+      econ: 0,
+      dipl: 10,
+      govt: 0,
+      scty: 0
+    }
+  },
+  {
+    question:
+      "The more feminine partner in the relationship should be the bottom.",
+    effect: {
+      econ: 0,
+      dipl: 0,
+      govt: 10,
+      scty: 0
+    }
+  },
+  {
+    question:
+      "The more masculine partner in the relationship should be the top.",
+    effect: {
+      econ: 0,
+      dipl: 0,
+      govt: 10,
+      scty: 0
+    }
+  },
+  {
+    question: "Femininity naturally pairs itself with masculinity.",
+    effect: {
+      econ: 0,
+      dipl: 0,
+      govt: 10,
+      scty: 0
+    }
+  },
+  {
+    question:
+      "In some ways femboys can be considered their own legitimate gender and pair well with traditional men.",
+    effect: {
+      econ: 0,
+      dipl: 0,
+      govt: 10,
+      scty: 0
+    }
+  },
+  {
+    question:
+      "Masculine people paired with eachother are preferable to 'traditional' gender roles.",
+    effect: {
+      econ: 0,
+      dipl: 0,
+      govt: -10,
+      scty: 0
+    }
+  },
+  {
+    question: "Tomboys, butch women and muscular women are based.",
+    effect: {
+      econ: 0,
+      dipl: 5,
+      govt: -10,
+      scty: 0
+    }
+  },
+  {
+    question: "The 'bottom' should be able or willing to powerbottom.",
+    effect: {
+      econ: 0,
+      dipl: 0,
+      govt: -5,
+      scty: 0
+    }
+  },
+  {
+    question:
+      "The dominant should equally prioritize the submissive's pleasure.",
+    effect: {
+      econ: 0,
+      dipl: 0,
+      govt: -5,
+      scty: 0
+    }
+  },
+  {
+    question:
+      "A good bottom services their top, even at the expense of their own pleasure.",
+    effect: {
+      econ: 0,
+      dipl: 0,
+      govt: 5,
+      scty: 0
+    }
+  },
+  {
+    question:
+      "A top should service their bottom, even at the expense of their own pleasure.",
+    effect: {
+      econ: 0,
+      dipl: 0,
+      govt: -5,
+      scty: 0
+    }
+  },
+  {
+    question: "There is no such thing as equality in the bedroom.",
+    effect: {
+      econ: 0,
+      dipl: 0,
+      govt: 10,
+      scty: 0
+    }
+  },
+  {
+    question:
+      "Partner's should always keep their singnificant other's sexual needs in mind.",
+    effect: {
+      econ: 0,
+      dipl: 0,
+      govt: -10,
+      scty: 0
+    }
+  },
+  {
+    question: "Monogamy is needed for a stable relationships.",
+    effect: {
+      econ: 0,
+      dipl: 0,
+      govt: 10,
+      scty: 0
+    }
+  },
+  {
+    question: "I would be okay with the occasional threesome.",
+    effect: {
+      econ: 0,
+      dipl: 0,
+      govt: -5,
+      scty: 0
+    }
+  },
+  {
+    question: "Polyamory or open relationships are ideal.",
+    effect: {
+      econ: 0,
+      dipl: 0,
+      govt: -10,
+      scty: 0
+    }
+  },
+  {
+    question: "I have kinks that require lifestyle changes, such as pet play.",
+    effect: {
+      econ: 0,
+      dipl: 0,
+      govt: -10,
+      scty: 0
+    }
+  },
+  {
+    question:
+      "Masculine people should engage in traditionally masculine activites (ex construction, repair).",
+    effect: {
+      econ: 0,
+      dipl: 0,
+      govt: 10,
+      scty: 0
+    }
+  },
+  {
+    question:
+      "Feminine people should engage in traditionally feminine activites (ex cooking, sowing).",
+    effect: {
+      econ: 0,
+      dipl: 0,
+      govt: 10,
+      scty: 0
+    }
+  },
+  {
+    question: "The masculine partner should be the breadwinner.",
+    effect: {
+      econ: 0,
+      dipl: 0,
+      govt: 10,
+      scty: 0
+    }
+  },
+  {
+    question: "The feminine partner should be the homemaker.",
+    effect: {
+      econ: 0,
+      dipl: 0,
+      govt: 10,
+      scty: 0
+    }
+  },
+  {
+    question: "All partners should contribute to the household income equally.",
+    effect: {
+      econ: 0,
+      dipl: 0,
+      govt: -10,
+      scty: 0
+    }
+  }
+];
